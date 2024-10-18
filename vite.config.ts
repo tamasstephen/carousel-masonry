59e7dd6d7eae379e0e535985580145c32f24c478
tamasstephen <kicsitnagyon@gmail.com>
2024-10-18 10:56:43 +0200

    chore: add type alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,7 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
